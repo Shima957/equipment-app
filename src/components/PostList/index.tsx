20a159bccf017da9bd37c7639ddf67b71726c7b4
Shima-Oh <shimasound04@gmail.com>
2022-01-04 20:07:45 +0900

    feat(postlist): 登録しているユーザーを表示するようにした

---
+++ src/components/PostList/index.tsx
@@ -0,0 +1,31 @@
+import { UserIcon } from '@heroicons/react/outline';
+import { User } from '@prisma/client';
+import Link from 'next/link';
+import { VFC } from 'react';
+
+type Props = {
+  users: User[];
+};
+
+const PostList: VFC<Props> = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <Link href={user.userId} key={user.id}>
+          <a className='w-1/5 flex flex-col items-center space-y-2 border-2 border-gray-300 rounded-xl p-4 shadow-sm transition-colors duration-200 hover:bg-gray-300'>
+            {user.avatorUrl ? (
+              <img src={user.avatorUrl} />
+            ) : (
+              <div className='p-2 rounded-full bg-slate-500 h-20 w-20'>
+                <UserIcon className='text-white' />
+              </div>
+            )}
+            <div>{user.displayName ?? user.userId}</div>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,42 @@
-const Home = () => {
-  return <div>hello</div>;
+import PostList from '@/components/PostList';
+import prisma from '@/lib/prisma';
+import { supabase } from '@/lib/supabase';
+import { User } from '@prisma/client';
+import { VFC } from 'react';
+
+type Props = {
+  users: User[];
+};
+
+const Home: VFC<Props> = ({ users }) => {
+  return <PostList users={users} />;
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const res = await prisma.user.findMany();
+  const users = res.map((res) => {
+    if (res.avatorUrl) {
+      return {
+        id: res.id,
+        userId: res.userId,
+        displayName: res.displayName,
+        avatorUrl: supabase.storage.from('avator').download(res.avatorUrl),
+      };
+    } else {
+      return {
+        id: res.id,
+        userId: res.userId,
+        displayName: res.displayName,
+        avatorUrl: null,
+      };
+    }
+  });
+
+  return {
+    props: {
+      users: users,
+    },
+  };
+};
