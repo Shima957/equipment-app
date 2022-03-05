675dd74352f2d2cd3d6cdf2f0e5ae30298898813
Shima-Oh <shimasound04@gmail.com>
2022-03-05 14:39:38 +0900

    refactor(input.stories): コンポネントをリスト表示するようにした

--- src/components/atoms/Input/Input.stories.tsx
+++ src/components/atoms/Input/Input.stories.tsx
@@ -1,14 +1,33 @@
-import { ComponentStoryObj } from '@storybook/react';
+import { ComponentProps, VFC } from 'react';
 import { Input } from './Input';
 
-type Story = ComponentStoryObj<typeof Input>;
+type InputProps = ComponentProps<typeof Input>;
+
+const renderInputs = (Input: VFC<InputProps>) => {
+  return (
+    <dl className='space-y-2'>
+      <dt>Text</dt>
+      <dd className='space-y-4 w-60'>
+        <Input type='text' defaultValue='text' />
+      </dd>
+      <dt>Email</dt>
+      <dd className='space-y-4 w-60'>
+        <Input type='email' defaultValue='sample@sample' />
+      </dd>
+      <dt>Password</dt>
+      <dd className='space-y-4 w-60'>
+        <Input type='password' defaultValue='sample' />
+      </dd>
+      <dt>Error</dt>
+      <dd className='space-y-4 w-60'>
+        <Input type='text' error />
+      </dd>
+    </dl>
+  );
+};
 
 export default { component: Input };
 
-export const Default: Story = {
-  args: { type: 'text', error: false },
-};
-
-export const Erorr: Story = {
-  args: { type: 'text', error: true },
+export const Inputs = () => {
+  return renderInputs(Input);
 };
